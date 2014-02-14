180c6e4c5c26f7d2f031f1451fad59b4208ad2ce
David <david.mowrer@comcast.net>
2014-02-14 13:04:55 -0700

    Assignments 208 209 210 211 216

--- 209-world-cities/db/seeds.rb
+++ 209-world-cities/db/seeds.rb
@@ -1,1 +1,18 @@
-# TODO: write this
+City.create! \
+    City_Name: "London, United Kingdom",
+    Country_Flag: "uk.png",
+    Time_Zone: "UTC+0000",
+	Poputation: "8,000,000"
+City.create! \
+    City_Name: "Paris, France",
+    Country_Flag: "france.png",
+    Time_Zone: "UTC+0100",
+    Poputation: "2,000,000"
+City.create! \
+    City_Name: "Shanghai, China",
+    Country_Flag: "prc.png",
+    Time_Zone: "UTC+0800",
+    Poputation: "14,000,000"
+
+
+
--- 211-elements-details/controller.rb
+++ 211-elements-details/controller.rb
@@ -4,4 +4,16 @@ get "/" do
   halt erb(:index)
 end
 
+#get "W/:x" do
+#	y =params["x"]
+#	id = z.find(y)
+#  halt erb(:show)
+#end
+
+#get "W/:x" do
+#	y =params["x"]
+#	id = x.find(y)
+#  halt erb(:show)
+#end
+
 # TODO: write handler for detail page route
