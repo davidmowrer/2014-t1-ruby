a986498f3cf6832cfda19a64451111c501838d7a
David Mowrer <david.mowrer@comcast.net>
2014-03-11 18:58:12 -0600

    Exercise 255

--- 255-prefs-in-database/controller.rb
+++ 255-prefs-in-database/controller.rb
@@ -1,26 +1,30 @@
 require './davinci-sinatra.rb'
 
 get "/" do
-  # TODO: If there is already a user_id saved in the session...
-  # TODO: ...then use it to load the @user instance variable.
-  # TODO: Otherwise just start a new empty User.
-
+  if session[:user_id] != nil
+    @user = User.find(session[:user_id])
+  else
+    @user = User.new
+  end
   halt erb(:index)
 end
 
 post "/" do
-  # TODO: If there is already a user_id saved in the session...
-  # TODO: ...then use it to load the @user instance variable.
-  # TODO: Otherwise just start a new empty User.
+  if session[:user_id] != nil
+    @user = User.find(session[:user_id])
+  else
+    @user = User.new
+  end
 
-  # TODO: Set the user's new preferences from POST params on the User
-  # TODO: Then save the User object
+  @user.color  = params["color"]
+  @user.letter = params["letter"]
+  @user.number = params["number"]
 
-  # TODO: Save that user ID to the session, in case we created a new
-  #       one. (If we already had a user ID in the session, it doesn't
-  #       hurt to overwrite it with the same value).
-
+  @user.save!
   redirect "/"
 end
 
-# TODO: Write handler for GET /logout
+get "/logout" do
+  session.clear
+  redirect "/"
+end
