710c7d62050eed3deafdf0825b49974210aff2d1
David Mowrer <david.mowrer@comcast.net>
2014-03-11 18:57:24 -0600

    exercise 254

--- 254-prefs-in-session/controller.rb
+++ 254-prefs-in-session/controller.rb
@@ -1,18 +1,25 @@
 require './davinci-sinatra.rb'
 
 get "/" do
-  # TODO: Retrieve three values stored in the session and use them
-  # to pre-fill form values in the view.
-
-  halt erb(:index)
-end
+  	@color  = session[:color]
+  	@letter = session[:letter]
+    @number = session[:number]
+  	halt erb(:index)
+  	end
 
 post "/" do
-  # TODO: Transfer values of POST params to become local variables.
-  # TODO: Save those local variables to the session so the values
-  #       will last.
+  	color  = params["color"]
+  	letter = params["letter"]
+ 	number = params["number"]
 
-  redirect "/"
+   	session[:color]  = color
+   	session[:letter] = letter
+   	session[:number] = number
+  	redirect "/"
 end
 
-# TODO: Write handler for GET /logout that empties the session
+get "/logout" do
+	session.clear
+	redirect "/"
+end
+
