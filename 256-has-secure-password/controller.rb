a9f1961eaaf0f9ef04b9f958f3709fe07030506f
David Mowrer <david.mowrer@comcast.net>
2014-03-16 14:58:09 -0600

    Exercise 256

--- 256-has-secure-password/controller.rb
+++ 256-has-secure-password/controller.rb
@@ -1,37 +1,58 @@
 require './davinci-sinatra.rb'
 
-# Users always expect a route at / so redirect them to where we want
 get "/" do
   redirect "/login"
 end
 
-# Setup GET route for registration form
 get "/users/new" do
-
-  # Define @user so new.html.erb view can use it to pre-fill in blanks
   @user = User.new
-
   halt erb(:new)
 end
 
-# TODO: Write POST route for handling registration form submissions
+post "/users/new" do
+  @user = User.new
+  @user.username              = params["username"]
+  @user.password              = params["password"]
+  @user.password_confirmation = params["password_confirmation"]
 
-# Setup GET route for login form
+  if @user.save == true
+    redirect "/login"
+  else
+    halt erb(:new)
+      end
+
+end
+
 get "/login" do
   halt erb(:login)
 end
 
-# TODO: Write POST route to handle login form submissions
+post "/login" do
+  username = params["username"]
+  password = params["password"]
+ 
+  found_user = User.find_by(username: username)
 
-# An example page that's accessible only after you're logged in
+  if found_user == nil 
+    @message = "Unknown username"
+    halt erb(:login)
+  elsif found_user.authenticate(password) == false  
+    @message = "Incorrect password"
+    halt erb(:login)
+  else
+    session[:user_id] = found_user.id
+    redirect "/welcome"
+  end 
+
+end
+
 get "/welcome" do
-  # Get the user_id out of the session
   user_id = session[:user_id]
-
-  # Look up the user row with that id
   @user = User.find(user_id)
-
   halt erb(:welcome)
 end
 
-# TODO: Write handler for GET /logout route, to clear the session.
+get "/logout" do
+  session.clear
+  redirect "/login"
+end
