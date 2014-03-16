ad1005e6ba8f034962fbc4a39b27fc4eac8229fa
David Mowrer <david.mowrer@comcast.net>
2014-03-16 14:59:25 -0600

    Exercise 259

--- 259-flash-now-login/controller.rb
+++ 259-flash-now-login/controller.rb
@@ -5,51 +5,30 @@ get "/" do
 end
 
 get "/login" do
-  # There's no error to show; not necessary, just being clear...
-  @error = nil
-
-  halt erb(:login)
+   @error = nil
+   halt erb(:login)
 end
 
 post "/login" do
-  # Load the values that user filled out for the username and password
-  # into variables.
   username = params["username"]
   password = params["password"]
 
-  # Look for a row in the users table with that same username
   found_user = User.find_by(username: username)
 
-  # If no row was found for that username...
   if found_user == nil
-    # Then set a message to show at the top of the page
-    @error = "Unknown username"
-
-    # And render the login page again
+    flash.now[:error] = "Unknown username"
     halt erb(:login)
-
-  # Otherwise, if the password was wrong...
   elsif found_user.authenticate(password) == false
-    # Then set a message to show at the top of the page
-    @error = "Incorrect password"
-
-    # And render the login page again
+    flash.now[:error] = "Incorrect password"
     halt erb(:login)
-
-  # Otherwise... (if the username and password was right)
   else
-    # Save that user's id into the session so we'll have it later
     session[:user_id] = found_user.id
-
-    # Redirect to the default page
     redirect "/welcome"
-  end # End if
-end # End handler
+  end
+end
 
 get "/welcome" do
-  # Load the user_id from the session
   user_id = session[:user_id]
-  # Load the user based on that id saved to the session
   @user = User.find(user_id)
   halt erb(:welcome)
 end
