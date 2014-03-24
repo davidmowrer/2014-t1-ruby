ca68ab956903d62a645ef4f98cab08318654d4df
David Mowrer <david.mowrer@comcast.net>
2014-03-24 00:13:30 -0600

    Exercise 307-people-and-tools/Gemfile.lock

--- 307-people-and-tools/db/seeds.rb
+++ 307-people-and-tools/db/seeds.rb
@@ -1,3 +1,9 @@
+Person.create! id: 1, name: "Eve"
+Person.create! id: 2, name: "Ed"
+Person.create! id: 3, name: "Tony"
 ActiveRecord::Base.connection.reset_pk_sequence! "people"
-
+Tool.create! name: "Bandsaw",     person_id: 1
+Tool.create! name: "Drill press", person_id: 1
+Tool.create! name: "Lathe",       person_id: 2
+Tool.create! name: "Planer",      person_id: nil
 ActiveRecord::Base.connection.reset_pk_sequence! "tools"
