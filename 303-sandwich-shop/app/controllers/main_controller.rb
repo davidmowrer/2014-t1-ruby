e37a122ec6453f6bc2b275cc491c89e211811436
David Mowrer <david.mowrer@comcast.net>
2014-03-24 00:06:30 -0600

    Exercise 303

--- 303-sandwich-shop/app/controllers/main_controller.rb
+++ 303-sandwich-shop/app/controllers/main_controller.rb
@@ -6,8 +6,34 @@ class MainController < ApplicationController
   end
 
   def place_order
-    # TODO: Create a new SubOrder and render the thank you page if successful
-    raise "TODO: start by removing this line"
+    @order = SubOrder.new
+    @order.sandwich_type   = params["sandwich_type"]
+    @order.bread_type      = params["bread_type"]
+    @order.is_twelve_inch  = params["is_twelve_inch"]
+    @order.has_extra_meat  = params["has_extra_meat"]
+    @order.has_chips       = params["has_chips"]
+  
+    total_price = 3.49
+
+    if @order.is_twelve_inch == true
+      total_price = total_price + 2.00
+    end
+
+    if @order.has_extra_meat == true
+      total_price = total_price + 0.49
+    end
+
+    if @order.has_chips == true
+      total_price = total_price + 0.99
+    end
+
+    @order.total_price = total_price
+  
+    if @order.save == true
+      render :thank_you and return
+    else
+      render :order_form and return
+    end
   end
 
   def list_orders
